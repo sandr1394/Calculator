1ef655373ff3e6ae7ec0b52d41b05f105581ce11
sandr1394 <46102529+sandr1394@users.noreply.github.com>
2019-04-20 00:46:10 +0700

    Refactoring

--- src/calculator/InternalRealisation.java
+++ src/calculator/InternalRealisation.java
@@ -5,29 +5,24 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 
 public class InternalRealisation implements ActionListener  {
-	Calculator parent; // Переменная ссылка на класс Calculator
+	Calculator parent;
 	double currentNumber, enteredNumber;
 	String selectedAction;
 
-	
 	InternalRealisation(Calculator parent) {
 		this.parent = parent;
 	}
 
 	public void actionPerformed(ActionEvent e) {
-		// Получение объекта, который вызвал действие
 		JButton clickedButton = (JButton) e.getSource();
-		//Получение текста из дисплея калькулятора
 		String dispFieldText = parent.getValue();
-		if (!dispFieldText.equals(""))
+		if (!dispFieldText.equals("")){
 			enteredNumber = Double.parseDouble(dispFieldText);
-
-		
+		}
 		if (clickedButton.getText().equals("+")) {
 			selectedAction = "+";
 			currentNumber = enteredNumber;
 			parent.setValue("");
-
 		} else if (clickedButton.getText().equals("-")) {
 			selectedAction = "-";
 			currentNumber = enteredNumber;
@@ -55,23 +50,16 @@ public class InternalRealisation implements ActionListener  {
 				currentNumber /= enteredNumber;
 				parent.setValue("" + currentNumber);
 			}
-		}
-
-		
-		else {
-
+		}else {
 			if (!clickedButton.getText().equals("=")) {
 				if ((clickedButton.getText().equals("C")))
 				{parent.eraseDisplay();}
 				else
 				{
-					// Отображение на экране текста нажатой кнопки
 					String clickedButtonLabel = clickedButton.getText();
 					parent.setValue(dispFieldText + clickedButtonLabel);
 				}
-
-			}
-				
+			}	
+		}
 	}
-}
 }
